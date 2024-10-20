177eee75a1631b6002ed409d70ada04bc93523ea
rock <rockgroupit777@gmail.com>
2024-10-20 17:01:08 +0700

    a

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,1 +1,2 @@
-export const resolvers ={};
+const resolvers = {};
+export {resolvers}
---
+++ server/src/schemas/index.ts
@@ -0,0 +1,16 @@
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import fs from 'fs';
+import path from 'path';
+
+// Function to read a GraphQL schema file
+const readSchemaFile = (filename: string) => {
+    return fs.readFileSync(path.join(__dirname, filename), { encoding: 'utf-8' });
+};
+
+// Merge schemas from files
+const typeDefs = mergeTypeDefs([
+    readSchemaFile('user.graphql'),
+    // readSchemaFile('post.graphql'), // Uncomment to include post schema
+]);
+
+export { typeDefs };

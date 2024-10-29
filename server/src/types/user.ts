01c48863387ca798d61ddc1ccaf078c280f232a0
rockgroupit777 <54498741+rockgroupit777@users.noreply.github.com>
2024-10-29 12:58:59 +0700

    Update user.ts

--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -1,1 +1,17 @@
+import { Document, Model } from "mongoose";
 
+export interface UserDocument extends Document {
+  email: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  avatar?: string; // Optional if not always provided
+  role: string;
+  permissions: string[]; // Changed to a more standard array syntax
+  matchesPassword(password: string): Promise<boolean>; // Improved syntax
+}
+
+export interface UserModel extends Model<UserDocument> {
+  hash(password: string): Promise<string>; // Improved syntax
+}

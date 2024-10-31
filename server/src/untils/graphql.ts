d50105e408aa6608d5b2f5bb349112ad22ea0b27
rockgroupit777 <54498741+rockgroupit777@users.noreply.github.com>
2024-10-31 10:31:27 +0700

    Update graphql.ts

--- server/src/untils/graphql.ts
+++ server/src/untils/graphql.ts
@@ -1,1 +1,26 @@
+import graphqlFields from "graphql-fields";
+import { GraphQLResolveInfo } from "graphql";
 
+interface FieldsMap {
+  [field: string]: {} | FieldsMap;
+}
+
+// Cache to memoize the result of getFieldsMap for each info object
+const fieldsMapCache = new WeakMap<GraphQLResolveInfo, FieldsMap>();
+
+const getFieldsMap = (info: GraphQLResolveInfo): FieldsMap => {
+  if (!fieldsMapCache.has(info)) {
+    const fields = graphqlFields(info, {}, { excludedFields: ["__typename"] });
+    fieldsMapCache.set(info, fields);
+  }
+  return fieldsMapCache.get(info)!;
+};
+
+export const hasSubfields = (info: GraphQLResolveInfo): boolean => {
+  return Object.values(getFieldsMap(info)).some(subfields => 
+    Object.keys(subfields).length > 0
+  );
+};
+
+export const getFieldsAsString = (info: GraphQLResolveInfo): string => 
+  Object.keys(getFieldsMap(info)).join(" ");

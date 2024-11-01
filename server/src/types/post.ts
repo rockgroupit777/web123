c9d94136b295c4ab6df697154deceeba7cafbb04
rockgroupit777 <54498741+rockgroupit777@users.noreply.github.com>
2024-11-01 11:07:06 +0700

    Update post.ts

--- server/src/types/post.ts
+++ server/src/types/post.ts
@@ -1,1 +1,14 @@
+import { Document } from "mongoose";
 
+export interface PostDocument extends Document {
+  title: string;
+  alias: string;
+  summary: string;
+  content: string;
+  cover: string;
+  photos: [string];
+  userId: UserDocument["_id"];
+  status: boolean;
+  likes: [UserDocument["_id"]];
+  commentStatus: boolean;
+}
